ae5d7f8c37bbb4d5455c26fd211a1ca00632d66b
Whiteshadow2664 <sjemma819@gmail.com>
2025-03-25 20:38:09 +0530

    Update bump.js

--- bump.js
+++ bump.js
@@ -49,7 +49,7 @@ module.exports.trackBump = async (message) => {
 };
 
 // ✅ Function to save cached bumps to DB at 22:30 IST daily
-cron.schedule("33 19 * * *", async () => {
+cron.schedule("46 20 * * *", async () => {
     console.log("⏳ Uploading bump data to database...");
 
     if (bumpCache.size === 0) {
